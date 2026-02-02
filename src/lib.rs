03aafdd0229e88cdca8ed4ab014ec34d34585504
Seungjin Kim <seungjin@duck.com>
2026-02-02 11:15:28 +0900

    image desc every min limit

--- src/lib.rs
+++ src/lib.rs
@@ -393,7 +393,7 @@ async fn get_image_descriptions(place: &mut Place) -> anyhow::Result<()> {
         let d3 = d2.get("description").unwrap().as_str().unwrap();
         photo.description = Some(d3.to_string());
         // It is free service. Let's give some buffer.
-        let short_sec = time::Duration::from_millis(4_300);
+        let short_sec = time::Duration::from_millis(60_000);
         thread::sleep(short_sec);
     }
     Ok(())
